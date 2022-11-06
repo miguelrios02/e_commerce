2d0af57302ea4b04d1a945d32594e388ea895dde
mianriga12 <mianriga@gmail.com>
2022-11-06 18:07:26 -0500

    first commit

---
+++ assets/main.js
@@ -0,0 +1,57 @@
+const clothes =[
+    {
+        id:0,
+        name: "Hoodies",
+        price: 1200,
+        stock:5,
+        urlImage:"./assets/img/featured1.png"
+    },
+    {
+        id:1,
+        name: "Shirts",
+        price: 1009,
+        stock:7,
+        urlImage:"./assets/img/featured2.png"
+    },
+    {
+        id:2,
+        name: "Sweatshirts",
+        price: 1300,
+        stock:2,
+        urlImage:"./assets/img/featured3.png"
+    }
+]
+
+
+const iconMenu = document.querySelector(".bx-menu");
+const menu = document.querySelector(".menu");
+
+iconMenu.addEventListener("click", function () {
+    console.log(menu.classList.toggle("menu-show"));
+});
+
+
+const contentClothes = document.querySelector(".contentClothes");
+
+let html=""
+clothes.forEach(({id, name, price, stock, urlImage})=>{
+   html +=`
+   <div class="clothes">
+        <div class="clothes__img">
+            <img src="${urlImage}" alt="${name}">
+        </div>
+        <div class="clothes__body">
+            <h3>${name}</h3>
+            <p><span>$${price}</span> - stock:${stock} </p>
+        </div>
+        <div class="clothes__options">
+            <button class="btn btn__add" id="">Agregar</button>
+        </div>
+</div>`
+});
+
+contentClothes.innerHTML= html
+
+console.log(contentClothes);
+
+
