992f90a8eff3f7d8fee5b5ecb6ce777f81f433e1
mianriga12 <mianriga@gmail.com>
2022-11-06 22:42:01 -0500

    target

--- assets/main.js
+++ assets/main.js
@@ -25,6 +25,8 @@ const clothes =[
 
 const iconMenu = document.querySelector(".bx-grid-alt");
 const menu = document.querySelector(".menu");
+const contentCartShopItems = document.querySelector(".contentCartShop__items");
+
 
 iconMenu.addEventListener("click", function () {
     console.log(menu.classList.toggle("menu-show"));
@@ -32,11 +34,12 @@ iconMenu.addEventListener("click", function () {
 
 const contentClothes = document.querySelector(".contentClothes");
 
+let objCartShop={};
 
-
-let html=""
-clothes.forEach(({id, name, price, stock, urlImage})=>{
-   html +=`
+function printClothes(){
+    let html="";
+    clothes.forEach(({id, name, price, stock, urlImage})=>{
+    html +=`
    <div class="clothes">
         <div class="clothes__img">
             <img src="${urlImage}" alt="${name}">
@@ -49,14 +52,80 @@ clothes.forEach(({id, name, price, stock, urlImage})=>{
             <button class="btn btn__add" id="${id}">Agregar</button>
         </div>
 </div>`
+contentClothes.innerHTML= html
 });
 
-contentClothes.innerHTML= html
+}
+printClothes();
 
-console.log(contentClothes);
+function printClothesInCart() {
+    let html = "";
+
+    const arrayCartShop = Object.values(objCartShop);
+
+    arrayCartShop.forEach(({ id, name, price, amo, urlImage }) => {
+        html += `
+            <div class="clothes">
+            <div class="clothes__img">
+                <img src="${urlImage}" alt="${name}">
+            </div>
+            <div class="clothes__body">
+                <h3>${name}</h3>
+                <p><span>$${price}</span> - cant: <strong>${amo}</strong></p>
+            </div>
+            <div class="clothes__options">
+                <button class="btn btn__rest" id="${id}">-</button>
+                <button class="btn btn__add" id="${id}">+</button>
+                <button class="btn btn__del" id="${id}">del</button>
+            </div>
+        </div>
+        `;
+    });
+
+    contentCartShopItems.innerHTML = html;
+}
+
+
+contentClothes.addEventListener("click",(e)=>{
+if(e.target.classList.contains("btn__add")){
+    const idClothes = Number(e.target.id);
+    const currentClothes= clothes.find((clothe) => clothe.id ==idClothes)
+    if(objCartShop[currentClothes.id])
+    {objCartShop[currentClothes.id].amo++;
+
+    }else{
+        objCartShop[currentClothes.id]= currentClothes;
+        objCartShop[currentClothes.id].amo=1;
+    }
+    printClothesInCart();
+}
+
+})
+contentCartShopItems.addEventListener("click",(m)=>{
+    if (m.target.classList.contains("btn__add")) {
+        const idClothe = Number(m.target.id);
+        if(objCartShop[idClothe].amo<objCartShop[idClothe].stock){
+            objCartShop[idClothe].amo++;
+        }
+        
+    }
+    if (m.target.classList.contains("btn__rest")) {
+        const idClothe = Number(m.target.id)
+        if(objCartShop[idClothe].amo>0){
+            objCartShop[idClothe].amo--;
+        }
+    }
+    if (m.target.classList.contains("btn__del")) {
+        const idClothe = Number(m.target.id)
+        delete objCartShop[idClothe];
+    }
+    printClothesInCart();
+    })
+
 const icontCart = document.querySelector(".bx-shopping-bag");
 const contentCartShop = document.querySelector(".contentCartShop");
 
+
 icontCart.addEventListener("click",()=>{
-    contentCartShop.classList.toggle("contentCartShop__show")
+    contentCartShop.classList.toggle("contentCartShop__show");
 })
