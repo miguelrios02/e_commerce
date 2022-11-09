1cd19d77bd64d034f5b29bf29664a842eb7ec979
mianriga12 <mianriga@gmail.com>
2022-11-08 20:09:58 -0500

    logica terminada

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 
-const clothes =[
+let clothes =[
     {
         id:0,
         name: "Hoodies",
@@ -23,6 +23,13 @@ const clothes =[
     }
 ]
 
+window.addEventListener("load",function (){
+    const loading = this.document.querySelector(".loading");
+    
+    setTimeout(()=>{
+        loading.style.display="none";
+    },1000);
+})
 
 const iconMenu = document.querySelector(".bx-grid-alt");
 const menu = document.querySelector(".menu");
@@ -30,32 +37,53 @@ const contentCartShopItems = document.querySelector(".contentCartShop__items");
 const bxMoon = document.querySelector(".bx-sun");
 const toggleLight =document.querySelector(".toggle__light");
 const contentClothes = document.querySelector(".contentClothes");
-
-iconMenu.addEventListener("click", function () {
-    console.log(menu.classList.toggle("menu-show"));
-});
+const contentCartShopTotal =document.querySelector(".contentCartShop__total");
+const icontCart = document.querySelector(".bag");
+const contentCartShop = document.querySelector(".contentCartShop");
+const countBag = document.querySelector(".count__bag");
 const navbarIcons= document.querySelector(".navbar__icons");
-    navbarIcons.addEventListener("click",(e)=>{
-   if(e.target.classList.contains("bx-moon")){
-    let html=""
-    html=`
-    <i class='bx bx-sun'></i>`
-    toggleLight.innerHTML= html
-   }
-   if( e.target.classList.contains("bx-sun")){
-    let html=""
-    html=`
-    <i class='bx bx-moon'></i>`
-    toggleLight.innerHTML= html
-   }
-});
-
 let objCartShop={};
 
+function addClothes(idClothe){
+    const currentClothes= clothes.find((clothe) => clothe.id ==idClothe)
+    if(currentClothes.stock=== objCartShop[idClothe].amo)
+    return alert("We don not have enough in stock");
+
+    objCartShop[currentClothes.id].amo++;
+}
+
+function deleteClothes(idClothe){
+     const op =confirm("seguro que quieres eliminar?")
+    if(op)delete objCartShop[idClothe];
+}
+function countProduct(){
+    const arrayCartShop = Object.values(objCartShop);
+    let suma =arrayCartShop.reduce((acum,curr)=>{
+        acum += curr.amo;
+        return acum;
+    },0);
+    countBag.textContent=suma;
+}
+function printTotal(){
+    const arrayCartShop = Object.values(objCartShop);
+if(!arrayCartShop.length) return (contentCartShopTotal.innerHTML= `<h3>carrito bacio</h3>`)
+
+let total =arrayCartShop.reduce((acum,curr)=>{
+    acum = curr.price * curr.amo;
+    return acum;
+},0);
+contentCartShopTotal.innerHTML=`
+<h3>${total}</h3>
+<button class="btn btn__buy">Comprar</button>
+`;
+}
 function printClothes(){
     let html="";
     clothes.forEach(({id, name, price, stock, urlImage})=>{
-    html +=`
+    const btnBuy =stock
+    ?`<button class="btn btn__add" id="${id}">Agregar</button>`
+    :`<button class="btn"> No disponible</button>`;
+        html +=`
     <div class="clothes ${name}" >
     <div class="clothes__img">
         <img src="${urlImage}" alt="${name}">
@@ -67,7 +95,7 @@ function printClothes(){
         </div>
         <h3>${name}</h3>
         <div class="clothes__options">
-            <button class="btn btn__add" id="${id}">Agregar</button>
+            ${btnBuy}
         </div>
      </div>   
     </div>`
@@ -75,8 +103,6 @@ contentClothes.innerHTML= html
 });
 
 }
-printClothes();
-
 
 function printClothesInCart() {
     let html = "";
@@ -103,53 +129,95 @@ function printClothesInCart() {
     });
 
     contentCartShopItems.innerHTML = html;
+    printTotal();
+    countProduct();
 }
-
-
 contentClothes.addEventListener("click",(e)=>{
-if(e.target.classList.contains("btn__add")){
-    const idClothes = Number(e.target.id);
-    const currentClothes= clothes.find((clothe) => clothe.id ==idClothes)
-    if(objCartShop[currentClothes.id])
-    {objCartShop[currentClothes.id].amo++;
-
-    }else{
-        objCartShop[currentClothes.id]= currentClothes;
-        objCartShop[currentClothes.id].amo=1;
+    if(e.target.classList.contains("btn__add")){
+        const idClothes = Number(e.target.id);
+        const currentClothes= clothes.find((clothe) => clothe.id ==idClothes)
+        if(objCartShop[currentClothes.id])
+        {addClothes(idClothes)
+    
+        }else{
+            objCartShop[currentClothes.id]= {...currentClothes};
+            objCartShop[currentClothes.id].amo=1;
+        }
+        printClothesInCart();
     }
-    printClothesInCart();
-}
-
-})
+    
+    })
 contentCartShopItems.addEventListener("click",(m)=>{
     if (m.target.classList.contains("btn__add")) {
-        const idClothe = Number(m.target.id);
-        if(objCartShop[idClothe].amo<objCartShop[idClothe].stock){
-            objCartShop[idClothe].amo++;
-        }
-        if(objCartShop[idClothe].amo==objCartShop[idClothe].stock){
-            alert("no hay mas elementos en el carrito")
-        }
-        
+            const idClothe = Number(m.target.id);
+            addClothes(idClothe)
+            
     }
     if (m.target.classList.contains("btn__rest")) {
         const idClothe = Number(m.target.id)
-        if(objCartShop[idClothe].amo>0){
+        if(objCartShop[idClothe].amo===1){
+            alert("quieres eliminar");
+            deleteClothes(idClothe);
+        }else{
             objCartShop[idClothe].amo--;
         }
     }
     if (m.target.classList.contains("btn__del")) {
         const idClothe = Number(m.target.id)
-        const op =confirm("seguro que quieres eliminar?")
-        if(op)delete objCartShop[idClothe];
+        deleteClothes(idClothe);
     }
     printClothesInCart();
-    })
-
-const icontCart = document.querySelector(".bag");
-const contentCartShop = document.querySelector(".contentCartShop");
-
-
+}) 
 icontCart.addEventListener("click",()=>{
     contentCartShop.classList.toggle("contentCartShop__show");
+})   
+
+contentCartShopTotal.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("btn__buy")){
+        const op = confirm("estas seguro de esto?")
+        if(op){
+           clothes= clothes.map((clothe)=>{
+                if(objCartShop[clothe.id]?.id === clothe.id){
+                    return{
+                        ...clothe,
+                        stock:clothe.stock - objCartShop[clothe.id].amo,
+                    };
+
+                }else{
+                    return clothe;
+
+                }
+            });
+            objCartShop={};
+            printClothes();
+            printClothesInCart();
+        }
+    }
 })
+printClothes();
+printTotal();
+
+iconMenu.addEventListener("click", function () {
+    console.log(menu.classList.toggle("menu-show"));
+});
+
+    navbarIcons.addEventListener("click",(e)=>{
+   if(e.target.classList.contains("bx-moon")){
+    let html=""
+    html=`
+    <i class='bx bx-sun'></i>`
+    toggleLight.innerHTML= html
+   }
+   if( e.target.classList.contains("bx-sun")){
+    let html=""
+    html=`
+    <i class='bx bx-moon'></i>`
+    toggleLight.innerHTML= html
+   }
+});
+
+
+
+
+
+
